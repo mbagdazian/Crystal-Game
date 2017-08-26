3435ab4a84ea93da5696b61ed784a0e1ad6c5d71
ewok1138 <mbagdazian90@gmail.com>
2017-08-25 21:42:24 -0700

    HW week 4

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+
+//winner
+var targetNumber = 150;
+//values
+var valCrystalOne = 25;
+var valCrystalTwo = 45;
+var valCrystalThree = 65;
+var valCrystalFour = 15;
+
+$('.auth-image').on("click", alert(clicks));
